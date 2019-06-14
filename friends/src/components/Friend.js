e7030a9e912bbe48127d419604c8af5f6448089e
arturolei <artie.lei@gmail.com>
2019-06-13 20:39:13 -0700

    MVP complete and some stretch done.

--- friends/src/components/Friend.js
+++ friends/src/components/Friend.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './friends.css';
+import PropTypes from 'prop-types';
 
 const Friend = props =>{
     return(
@@ -7,10 +8,18 @@ const Friend = props =>{
             <h2>{props.friend.name}</h2>
             <p>{props.friend.age}</p>
             <p>{props.friend.email}</p>
-            <button onClick={()=>props.deleteFriend(props.friend.id)}>Delete</button>
+            <button onClick={()=>props.deleteFriend(props.friend.id)}>Delete Friendo</button>
             <button onClick={()=>props.setUpdateMode(props.friend)}>Update Friendo</button>
         </div>
     );
+}
+
+Friend.propTypes = {
+    friend: PropTypes.shape({
+        name: PropTypes.number.isRequired,
+        age: PropTypes.number.isRequired,
+        email: PropTypes.string.isRequired
+    })
 }
 
 export default Friend;
